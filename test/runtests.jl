eff00a68ee4bf92ae21fe8492846a45162f04395
Alexander Plavin <alexander@plav.in>
2024-01-01 00:00:00 +0000

    more Aqua tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -206,8 +206,6 @@ import Aqua
 import CompatHelperLocal as CHL
 @testset begin
     CHL.@check()
+    Aqua.test_all(MonteCarloTesting, ambiguities=false, project_toml_formatting=false)
     Aqua.test_ambiguities(MonteCarloTesting, recursive=false)
-    Aqua.test_unbound_args(MonteCarloTesting)
-    Aqua.test_undefined_exports(MonteCarloTesting)
-    Aqua.test_stale_deps(MonteCarloTesting)
 end
